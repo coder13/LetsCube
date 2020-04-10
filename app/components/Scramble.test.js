120f9ec69117f7fb1a5c4c050fe37b67e8a49454
Caleb Hoover <choover11@gmail.com>
2020-04-10 14:39:57 -0700

    added another small test

---
+++ app/components/Scramble.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Scramble from './Scramble';
+
+const scramble = 'test';
+
+it('renders without crashing', () => {
+  shallow(<Scramble scrambles={[scramble]} />);
+});
+
+
+it('renders 3x3 text correctly', () => {
+  const component = shallow(<Scramble event="333" scrambles={[scramble]} />);
+
+  expect(component.text()).toEqual(scramble);
+});
