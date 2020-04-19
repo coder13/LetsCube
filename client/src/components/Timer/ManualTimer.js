c967cb9382a6a37295852bf40ef288a17f435789
Caleb Hoover <choover11@gmail.com>
2020-04-18 17:18:58 -0700

    parses numerical times

--- client/src/components/Timer/ManualTimer.js
+++ client/src/components/Timer/ManualTimer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Input from '@material-ui/core/Input';
@@ -10,27 +10,81 @@ const useStyles = makeStyles((theme) => ({
     ...initialStyles(theme).root,
   },
   input: {
-    maxWidth: '200px',
+    maxWidth: '300px',
     margin: 'auto',
+    fontSize: '4em',
+    backgroundColor: theme.palette.grey[300],
+  },
+  inputProps: {
+    textAlign: 'center',
+    '&:focus': {
+      backgroundColor: theme.palette.grey[400],
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: theme.borderRadius,
+    },
   },
 }));
 
-function ManualTimer() {
+function parseNumericalTime(time) {
+  const seconds = time % 10000;
+  const minutes = (time - seconds) / 10000;
+  return (seconds + minutes * 60 * 100) * 10;
+}
+
+function parseTime(inputTime) {
+  const time = +inputTime;
+
+  if (Number.isNaN(time)) {
+    return false;
+  }
+
+  return parseNumericalTime(time);
+}
+
+function ManualTimer({ disabled, onSubmitTime }) {
   const classes = useStyles();
+  const [timeInput, setTimeInput] = React.useState('');
+  const [DNF] = React.useState(false);
+  const [AUF] = React.useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const time = parseTime(timeInput);
+    if (time) {
+      onSubmitTime({
+        time,
+        penalties: {
+          DNF,
+          AUF,
+        },
+      });
+      setTimeInput('');
+    }
+  };
 
   return (
     <Box className={classes.root}>
-      <Input
-        className={classes.input}
-        value="0"
-      />
+      <form onSubmit={onSubmit}>
+        <Input
+          className={classes.input}
+          component="textarea"
+          inputProps={{
+            className: classes.inputProps,
+          }}
+          disabled={disabled}
+          value={timeInput}
+          onChange={(e) => setTimeInput(e.target.value)}
+          disableUnderline
+          fullWidth
+        />
+      </form>
     </Box>
   );
 }
 
 ManualTimer.propTypes = {
-  // onSubmitTime: PropTypes.func.isRequired,
-  // disabled: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  onSubmitTime: PropTypes.func.isRequired,
   // focused: PropTypes.bool.isRequired,
 };
 
