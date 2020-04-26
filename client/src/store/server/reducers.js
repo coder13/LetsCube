fbc94ff6cca4c9c2cf2cfb8735d93a72a6c9fbfb
Bill Fan <1472451850@qq.com>
2020-04-26 15:28:53 -0400

    Implemented reducer

--- client/src/store/server/reducers.js
+++ client/src/store/server/reducers.js
@@ -1,18 +1,18 @@
 import { USER_COUNT_UPDATED } from './actions';
 
 const INITIAL_STATE = {
-    user_count = 0,
-}
+  user_count: 0,
+};
 
 const reducers = {
-    [USER_COUNT_UPDATED]: (state, action) => ({...state, user_count: action.userCount}),
-}
+  [USER_COUNT_UPDATED]: (state, action) => ({ ...state, user_count: action.userCount }),
+};
 
-serverReducer(state = INITIAL_STATE, action) {
-    if(reducers[action.type]) {
-        return reducers[action.type](state, action);
-    }
-    return state;
+function serverReducer(state = INITIAL_STATE, action) {
+  if (reducers[action.type]) {
+    return reducers[action.type](state, action);
+  }
+  return state;
 }
 
 export default serverReducer;
