9a5ca5a7b738bbf0181d4d45e333a7c2b9f900a2
Caleb <choover11@gmail.com>
2021-01-06 07:31:17 -0800

    DNFs result in 0 points

--- client/src/lib/stats.js
+++ client/src/lib/stats.js
@@ -117,7 +117,7 @@ export default function (_attempts, users) {
 
 // 8 place spread 10/8/6/5/4/3/2/1
 
-const calculateGrandPrixRules = (place) => {
+const calculateGrandPrixRules = (result, place) => {
   switch (place) {
     case 0: return 10;
     case 1: return 8;
@@ -132,14 +132,18 @@ const calculateGrandPrixRules = (place) => {
 };
 
 export const calculatePointsForAttempt = (type, results) => {
-  const sortedResults = Object.keys(results).map((key) => ({
-    id: key,
-    time: getTimeOrDNF(results[key]),
-  })).sort((a, b) => sort(a.time, b.time)).map((result, index) => ({
-    id: result.id,
-    time: result.time,
-    points: calculateGrandPrixRules(index),
-  }));
+  const sortedResults = Object.keys(results)
+    .map((key) => ({
+      id: key,
+      time: getTimeOrDNF(results[key]),
+    }))
+    .filter((result) => result.time > 0)
+    .sort((a, b) => sort(a.time, b.time))
+    .map((result, index) => ({
+      id: result.id,
+      time: result.time,
+      points: calculateGrandPrixRules(result, index),
+    }));
 
   const points = {};
 
