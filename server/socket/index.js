f3f567ad585391b331b215695939eac949e6c9d3
Caleb Hoover (prod) <choover11@gmail.com>
2021-01-07 22:44:14 +0000

    fixed bug with client expecting admin of room to be sent with room list but wasn't

--- server/socket/index.js
+++ server/socket/index.js
@@ -8,7 +8,7 @@ const Protocol = require('../../client/src/lib/protocol.js');
 const { User, Room } = require('../models');
 const ChatMessage = require('./ChatMessage');
 
-const publicRoomKeys = ['_id', 'name', 'event', 'usersLength', 'private', 'type', 'requireRevealedIdentity', 'startTime', 'started'];
+const publicRoomKeys = ['_id', 'name', 'event', 'usersLength', 'private', 'type', 'admin', 'requireRevealedIdentity', 'startTime', 'started'];
 const privateRoomKeys = [...publicRoomKeys, 'users', 'competing', 'waitingFor', 'banned', 'attempts', 'admin', 'accessCode', 'inRoom', 'registered', 'nextSolveAt'];
 
 // Data for people not in room
