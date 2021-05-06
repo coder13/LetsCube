c50e00714b2644063d83c12db3b22908fa8ba2c3
Caleb <choover11@gmail.com>
2021-05-05 17:51:17 -0700

    admin room cards scroll now

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -13,10 +13,19 @@ import { fetchAdminData } from '../../store/admin/actions';
 
 const useStyles = makeStyles(() => ({
   root: {
-    padding: '1em',
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    height: 0,
+  },
+  roomContainer: {
+    overflow: 'auto',
   },
   card: {
     marginTop: '1em',
+  },
+  refetchButton: {
+    margin: '1em',
   },
 }));
 
@@ -41,40 +50,43 @@ function Admin({ rooms }) {
         color="primary"
         disableElevation
         onClick={() => dispatch(fetchAdminData())}
+        className={classes.refetchButton}
       >
         Refetch Data
       </Button>
-      {rooms.map((room) => (
-        <Card key={room.id} className={classes.card}>
-          <CardContent>
-            <Typography variant="h4" component="h2">{room.name}</Typography>
-            <Typography>
-              {`id: ${room.id}`}
-              <IconButton
-                size="small"
-                onClick={() => copyToClipboard(room.id)}
-              >
-                <FilterNoneIcon />
-              </IconButton>
-            </Typography>
-            <Typography>{`admin: ${renderUser(room.admin)}`}</Typography>
-            <Typography>{`owner: ${renderUser(room.owner)}`}</Typography>
-            <Typography>{`accessCode: ${room.accessCode}`}</Typography>
-            <Typography>{`private: ${room.private}`}</Typography>
-            {room.private && <Typography>{`password: ${room.password}`}</Typography>}
-            <Typography>{`users: [${room.users.map(renderUser).join(', ')}]`}</Typography>
-            <Typography variant="h5" component="h3">Users in Room: </Typography>
-            <div style={{ padding: '1em' }}>
-              {room.userSocketsInRoom.length > 0
-                ? room.userSocketsInRoom.map((user) => (
-                  <Typography key={user.id}>{`${renderUser(user)}: [${user.sockets.join(', ')}]`}</Typography>
-                ))
-                : <Typography>None</Typography>}
-            </div>
-            <Typography>{`Expires at: ${room.expireAt}`}</Typography>
-          </CardContent>
-        </Card>
-      ))}
+      <div className={classes.roomContainer}>
+        {rooms.map((room) => (
+          <Card key={room.id} className={classes.card}>
+            <CardContent>
+              <Typography variant="h4" component="h2">{room.name}</Typography>
+              <Typography>
+                {`id: ${room.id}`}
+                <IconButton
+                  size="small"
+                  onClick={() => copyToClipboard(room.id)}
+                >
+                  <FilterNoneIcon />
+                </IconButton>
+              </Typography>
+              <Typography>{`admin: ${renderUser(room.admin)}`}</Typography>
+              <Typography>{`owner: ${renderUser(room.owner)}`}</Typography>
+              <Typography>{`accessCode: ${room.accessCode}`}</Typography>
+              <Typography>{`private: ${room.private}`}</Typography>
+              {room.private && <Typography>{`password: ${room.password}`}</Typography>}
+              <Typography>{`users: [${room.users.map(renderUser).join(', ')}]`}</Typography>
+              <Typography variant="h5" component="h3">Users in Room: </Typography>
+              <div style={{ padding: '1em' }}>
+                {room.userSocketsInRoom.length > 0
+                  ? room.userSocketsInRoom.map((user) => (
+                    <Typography key={user.id}>{`${renderUser(user)}: [${user.sockets.join(', ')}]`}</Typography>
+                  ))
+                  : <Typography>None</Typography>}
+              </div>
+              <Typography>{`Expires at: ${room.expireAt}`}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </Paper>
   );
 }
